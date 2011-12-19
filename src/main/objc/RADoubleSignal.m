15a8e60a39f40faa75a00d3730807174fc4cd8ca
Charlie Groves <charlie.groves@gmail.com>
2011-12-19 15:52:54 -0800

    Add RADoubleSignal
    Probably worth doing this for all the primitive types, but maybe I'll figure out a way to do it with less code.

---
+++ src/main/objc/RADoubleSignal.m
@@ -0,0 +1,34 @@
+//
+// React - a library for functional-reactive-like programming
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/react/blob/master/LICENSE
+
+#import "RADoubleSignal.h"
+#import "RAReactor+Protected.h"
+#import "RAConnection+Package.h"
+
+@implementation RADoubleSignal
+- (void) emitEvent:(double)event {
+    for (RAConnection *cur = [self prepareForEmission]; cur != nil; cur = cur->next) {
+        ((RADoubleSlot)cur->block)(event);
+        if (cur->oneShot) [cur disconnect];
+    }
+    [self finishEmission];
+}
+
+- (RAConnection*) connectSlot:(RADoubleSlot)block {
+    return [self withPriority:RA_DEFAULT_PRIORITY connectSlot:block];
+}
+
+- (RAConnection*) withPriority:(int)priority connectSlot:(RADoubleSlot)block {
+    return [self connectConnection:[[RAConnection alloc] initWithBlock:[block copy] atPriority:priority onReactor:self]];
+}
+
+- (RAConnection*) connectUnit:(RAUnitBlock)block {
+    return [self withPriority:RA_DEFAULT_PRIORITY connectUnit:block];
+}
+
+- (RAConnection*) withPriority:(int)priority connectUnit:(RAUnitBlock)block {
+    return [self withPriority:priority connectSlot:^(double event) { block(); }];
+}
+@end
