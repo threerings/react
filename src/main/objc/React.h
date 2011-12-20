774d45e17d83400f1e812150f12dfa69e61fac1f
Charlie Groves <charlie.groves@gmail.com>
2011-12-20 14:47:22 -0800

    Add full library header

---
+++ src/main/objc/React.h
@@ -0,0 +1,10 @@
+//
+// React - a library for functional-reactive-like programming
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/react/blob/master/LICENSE
+
+#import "RAConnection.h"
+#import "RAConnectionGroup.h"
+#import "RADoubleSignal.h"
+#import "RASignal.h"
+#import "RAUnitSignal.h"
