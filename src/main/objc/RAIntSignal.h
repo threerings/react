9bca1085f54ecfca3a97c5ad05c0c3de9ace7925
Tim Conkling <tconkling@gmail.com>
2012-01-19 14:51:23 -0800

    Signal classes for BOOL, float, and int

---
+++ src/main/objc/RAIntSignal.h
@@ -0,0 +1,30 @@
+//
+// React - a library for functional-reactive-like programming
+// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
+// http://github.com/threerings/react/blob/master/LICENSE
+
+#import <Foundation/Foundation.h>
+#import "RAReactor.h"
+
+typedef void (^RAIntSlot)(int);
+
+@interface RAIntSignal : RAReactor
+/** @name Emission */
+
+/** Emits the supplied value to all connected slots. */
+- (void) emitEvent:(int)event;
+
+/** @name Connection */
+
+/** Connects the given block to receieve emissions from this signal at the default priority.  */
+- (RAConnection*) connectSlot:(RAIntSlot)block;
+
+/** Connects the given block at the given priority.  */
+- (RAConnection*) withPriority:(int)priority connectSlot:(RAIntSlot)block;
+
+/** Connects the given unit at the default priority.  */
+- (RAConnection*) connectUnit:(RAUnitBlock)block;
+
+/** Connects the given unit at the given priority.  */
+- (RAConnection*) withPriority:(int)priority connectUnit:(RAUnitBlock)block;
+@end
