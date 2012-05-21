b62378445b57d456f04a5ce0cdca079f10e35f84
Michael Bayne <mdb@samskivert.com>
2012-05-21 14:54:43 -0700

    Allow a UnitSlot to be used as a Runnable.

--- src/main/java/react/UnitSlot.java
+++ src/main/java/react/UnitSlot.java
@@ -7,9 +7,11 @@ package react;
 
 /**
  * A {@link Slot} for use when the type of emitted signal is ignored. As slots are contravariant,
- * this slot may be wired to any signal without bothering to determine its type.
+ * this slot may be wired to any signal without bothering to determine its type. Instances of this
+ * class also implement {@link Runnable} so that they may be invoked by code that expects runnables
+ * rather than no-arg slots.
  */
-public abstract class UnitSlot extends Slot<Object>
+public abstract class UnitSlot extends Slot<Object> implements Runnable
 {
     // if you're using unit slot, you're not allow to see the event
     @Override public final void onEmit (Object event) {
@@ -20,4 +22,9 @@ public abstract class UnitSlot extends Slot<Object>
      * Called when a signal to which this slot is connected has emitted an event.
      */
     public abstract void onEmit ();
+
+    @Override // from Runnable
+    public void run () {
+        onEmit();
+    }
 }
