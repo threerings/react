e0561bdd8924e94a36d4f96764f9ca5a0df96e24
Michael Bayne <mdb@samskivert.com>
2014-06-12 10:43:00 -0700

    Retiring Obj-C version of React.
    It's not being maintained, so I'd rather retire it now before it causes
    troubles.

--- src/main/objc/RABoolReactor+Protected.h
+++
@@ -1,8 +0,0 @@
-//
-// React - a library for functional-reactive-like programming
-// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
-// http://github.com/threerings/react/blob/master/LICENSE
-
-@interface RABoolReactor (protected)
-- (void)dispatchEvent:(BOOL)event;
-@end
--- src/main/objc/RAFloatReactor+Protected.h
+++
@@ -1,8 +0,0 @@
-//
-// React - a library for functional-reactive-like programming
-// Copyright (c) 2011, Three Rings Design, Inc. - All rights reserved.
-// http://github.com/threerings/react/blob/master/LICENSE
-
-@interface RAFloatReactor (protected)
-- (void)dispatchEvent:(float)event;
-@end
